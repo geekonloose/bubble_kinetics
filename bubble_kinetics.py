315bca5025913b1d8f8f1ad50a7ad92446a568f0
parth3791 <30520900+parth3791@users.noreply.github.com>
2017-08-07 16:46:10 +0530

    change,turtle added,graph plotted
    change,turtle added,graph plotted

--- bubble_kinetics.py
+++ bubble_kinetics.py
@@ -12,7 +12,7 @@ Created on Sat Jul 29 17:10:21 2017
 
 import numpy as np
 import math
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 '''
 #%% Variable Initiallisation
 
@@ -53,8 +53,9 @@ sigmal = 0
 #%% Program Starts here
 '''
 
-number = int(1E4)
+number = int(1E3)
 dt = 1E-2
+
 #%% data for rate of temperature change
 
 T = np.zeros(number)
@@ -65,14 +66,14 @@ Tb = 1154.1 # Boiling point of sodium in kelvine
 Ts = 800 # sodium pool temperature
 cp = 1284.4 #kJ/kg
 
-
+lg = 4197E3
 
 
 #%% Data for change in velocity
 
 v = np.zeros(number) #bubble rising velocity m/s
 t = np.zeros(number)
-x = np.zeros(number)
+y = np.zeros(number)
 rhol = np.zeros(number)
 rhog = np.zeros(number)
 
@@ -87,32 +88,83 @@ H = 13
 
 #%% Input Parameter Needed
 
-N = 1E25
-Db[0]
-Pb[0]
-
-
-
-
-
-for i in range(number-1):
+N = 0.23E23 #chack
+h = 1E1 #check
+Pb[0] = 1E5 #check
+T[0]= 3000 #check
+rhog[:] = 200 #check the value
+rhol[0]=949-0.223*(T[0]-273.15)-1.7E-5*(T[0]-273.15)**2
+print('sodium density \t{:0.3e}'.format(rhol[0]))
+Db[0] =N*k*T[0]/(3.14*Pb[0])
+print('bubble diameter \t{:0.3e}'.format(Db[0]*1E3))
+#Db[0] = 2
+v[0] = 0 #check value
+for i in range(number-3):
 
     t[i] = i*dt
+    y[i] = (i)*dt*v[i]
 
-    T[i+1] = T[i] + dt* (mass_of_vap_sod*cp*(Tb-Ts)-h*3.14*Db[i]**2 * (T[i]-Ts))
-
-    rhol[i+1] =
-
-    v[i+1] = v[i] + dt*(6/(rhol[i]*3.14*Db[i]**3))*(  (3.14*Db**3 *g*(rhol[i]-rhog[i]))/6 - 12*3.14*Db[i]*v[i])
-
-    x[i+1] = t[i+1]*v[i+1]
-
-    Pb[i+1] = P0 + rhol[i+1]*g*(H-x[i+1])
-
-    Db[i+1] = N*k*T[i+1]/(3.14*Pb[i])
+    Pb[i] = P0 + rhog[i]*g*(H-y[i])
+    Db[i] = (N*k*T[i]/(3.14*Pb[i]))**(1/3.0)
 
 
 
+    T[i+1] = T[i] + dt* (0-h*3.14*Db[i]**2 * (T[i]-Ts))
+
+    rhol[i+1] = 949-0.223*(T[i+1]-273.15)-1.7E-5*(T[i+1]-273.15)**2
+
+    v[i+1] = v[i] + dt*(6/(rhol[i]*3.14*Db[i]**3))*(  (3.14*Db[i]**3 *g*(rhol[i]-rhog[i]))/6 - 12*3.14*Db[i]*v[i])
+plt.figure()
+plt.plot(t[:-4],Db[:-4],label='diameter',color='blue')
+plt.xlabel('time(seconds)',color='blue',size=14)
+plt.ylabel('diameter (meter)',color='blue',size=14)
+plt.legend()
+plt.savefig('diameter')
+plt.figure()
+plt.plot(t[:-4],T[:-4],label = 'Temperature',color='blue')
+plt.xlabel('time',color='blue',size=14)
+plt.ylabel('Temp (K)',color='blue',size=14)
+plt.legend()
+plt.savefig('temperature')
+plt.figure()
+plt.plot(t[:-4],Pb[:-4],label='pressure',color='blue')
+plt.xlabel('time(seconds)',color='blue',size=14)
+plt.ylabel('Pressure (Pa)',color='blue',size=14)
+plt.legend()
+plt.savefig('pressure')
+plt.figure()
+plt.plot(t[:-4],v[:-4],label='velocity',color='blue')
+plt.xlabel('time(second)',color='blue',size=14)
+plt.ylabel('velocity(m/s)',color='blue',size=14)
+
+plt.legend()
+plt.savefig('velocity')
+#convert meter to cm
+Db =(Db*1E2)
+y = y*1E2-250
+#convert array to list
+Db = list(Db)
+y = list(y)
+import turtle
+
+def draw_circle(turtle, color, size, x, y):
+    turtle.penup()
+    turtle.color(color)
+
+    turtle.goto(x,y)
+    turtle.pendown()
+
+    turtle.circle(size)
+
+
+turtle.shape("circle")
+#turtle.resizemode("user")
+turtle.shapesize(1E-2,1E-2)
+#tommy.speed(7)
+scale = 100
+# Draw three circles:
+for i in range(int(len(y)/scale)):
+    draw_circle(turtle, "green", Db[scale*i], 25, y[scale*i])
 
 
 
