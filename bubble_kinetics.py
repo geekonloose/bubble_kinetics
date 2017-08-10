7a40ab497c8d532cc5cf27ba76038552e5035055
parth3791 <30520900+parth3791@users.noreply.github.com>
2017-08-10 10:13:09 +0530

    Final working form
    Still the density inside the bubble is kept constant. And hence the number density is also constant.

--- bubble_kinetics.py
+++ bubble_kinetics.py
@@ -13,48 +13,11 @@ from IPython import get_ipython
 get_ipython().magic('reset -sf')
 import numpy as np
 import math
+import time
+import os
 from matplotlib import pyplot as plt
-'''
-#%% Variable Initiallisation
 
-T = 800 #absulute temperature in kelvin
-H = 13 # pool height in m
-Na = 6.022E23 # Avogrado number
-P = 1E5 # Absolute pressure in pascal
-k = 1.3807E-23 #boltzman constant in J/k
-dp = 1E-6 #particle diameters
-lembda = 82.06*T/(float(sqrt(2))*3.14*Na*P) #mean free path of a gas molecule
-c = 1 + (2*lembda/dp) * (1.257 + 0.4 * math.exp(-0.55*dp/lembda)) #cunningham slip correction
-
-dp = 1E-6 #particle diameter
-dg = 0.5E-6 #effective diameter fo a gas molecular
-mug = 1.87E-5 #gas viscosity
-#%% viscosity of sodium
-t  = 373-273
-Al = 1.2162E-5
-Bl = 949 - 0.223*( t)-1.75E-5*(t)**2
-Cl = 0.6976
-mul = Bl**(1.0/3) * Al * math.exp(Bl*Cl/(t+273.15)) #liquid viscosity
-rhol =  949 - 0.223*t - 1.75E-5 * t**2 #density of liquid kg/m3
-rhop = 1 #density of particle kg/m3
-
-Db =[1E-3] #bubble diameter
-E = 1 #bubble evventricity
-g = 9.8 #
-
-Pi = 2E5 #pressure inside bubble
-Po = 1E5 #pressure outside bubble
-
-theta = k*T*c/(3*3.14*mug*dp)
-tau = rhop*dp**2 *c /(18*mug)
-
-
-x = [1,2,3,4,5,6,7,8,9,10]
-sigmal = 0
-#%% Program Starts here
-'''
-
-number = int(1E3)
+number = int(1E5)
 dt = 1E-2
 
 #%% data for rate of temperature change
@@ -85,49 +48,77 @@ Pb = np.zeros(number)
 k = 1.380648813131313131E-23 #Boltzman constant J/K
 g = 9.8 # gravitational acceleration
 P0 = 1E5 # Atmosperic pressure 1 bar = 1E5 pascals
-H = 13 # height of the pool
+H = 5 # height of the pool
 
 #%% Input Parameter Needed
 
 N = np.zeros(number) #chack
 h = 1E1 #check
 M = np.zeros(number)
-M[0] = 1E-2 #initial mass of aerosols in bubble
+M[0] = 1E-1 #initial mass of aerosols in bubble
 Pb[0] = 1E5 #check
 T[0]= 3000 #check
 
 rhol[0]=949-0.223*(T[0]-273.15)-1.7E-5*(T[0]-273.15)**2 #density of sodium
-print('sodium density \t{:0.3e}'.format(rhol[0]))
-
-print('bubble diameter \t{:0.3e}'.format(Db[0]*1E3))
+Pb[0] =  P0 + rhol[0]*g*(H)
 #Db[0] = 2
 v[0] = 0 #check value
 k = 1.3807E-23 #boltzman constant in J/k
-dp = 1E-6 #particle diameters
+dp = 5E-6 #particle diameters
 Na = 6.022E23 #avogrado number
-N[0] = 1E24
-N[:] = 1E24
+
+N[:] = 1E21
 #Db[0] = 1E-1 #initial size of bubble
+
 Db[0]= (N[0]*k*T[0]/(3.14*Pb[0]))**(1/3.0)
 rhog[0] = M[0]/(0.144*3.14*Db[0]**3) #density of the gas in bubble
+
 
 mug = 1.87E-5 #gas viscosity
 rhop = M[0]/(0.144*3.14*Db[0]**3) #density of particle
 
+v[0] = 0.01
+rhog[:] = 5E-2
+print('sodium density \t{:0.3e}'.format(rhol[0]))
+
+print('bubble diameter \t{:0.3e}'.format(Db[0]*1E3))
+par = np.zeros(number)
+
+RF = np.zeros(number)
 
 for i in range(number-3):
-    print(i)
+
+#    print(i)
+
     t[i] = i*dt
+
     y[i] = (i)*dt*v[i]
 
-    Pb[i] = P0 + rhog[i]*g*(H-y[i]) # pressure equation
+    Pb[i] = P0 + rhol[i]*g*(H-y[i]) # pressure equation
+#
 #    Db[0]= (N[0]*k*T[0]/(3.14*Pb[0]))**(1/3.0)
+
+
+
+#    N[i] = rhog[i]*Na/23
+
     if i > 0 :
         Db[i] = (N[i]*k*T[i]/(3.14*Pb[i]))**(1/3.0) # diameter equation
+
+
+    T[i+1] = T[i] + dt* (0-h*3.14*Db[i]**2 * (T[i]-Ts)) # temperature equation
+
+
+    rhol[i+1] = 949-0.223*(T[i+1]-273.15)-1.7E-5*(T[i+1]-273.15)**2 #density of sodium
+
+    v[i+1] = v[i] + dt*(6/(rhol[i]*3.14*Db[i]**3))*(  (3.14*Db[i]**3 *g*(rhol[i]-rhog[i]))/6 - 12*3.14*Db[i]*v[i]) #velocity of bubble
+
     lembda = 82.06*T[i]/(float(math.sqrt(2))*3.14*Na*Pb[i]) #mean free path of a gas molecule
 
     cunn = 1 + (2*lembda/dp) * (1.257 + 0.4 * math.exp(-0.55*dp/lembda)) #cunningham slip correction
+
     theta = k*T[i]*cunn/(3*3.14*mug*dp)
+
     tau = rhop*dp**2 *cunn /(18*mug)
 
     diff = 1.8*(8*theta/(v[i]*Db[i]**3)) #diffusion coefficient
@@ -137,22 +128,19 @@ for i in range(number-3):
     inertial_impaction = 18*v[i]*tau/Db[i]**2 # inertial impaction coefficient
 
     c = diff + sedimentation + inertial_impaction
-    if i >0:
-        M[i] =  M[0] * math.exp(-c*y[i])
-        rhog[i] = M[i]/(0.144*3.14*Db[i]**3 )
 
+    dy = dt*v[i]
+    par[i] = dy*(-c*M[i])
+    M[i+1] = M[i] + par[i]
 
-    T[i+1] = T[i] + dt* (0-h*3.14*Db[i]**2 * (T[i]-Ts)) # temperature equation
+    RF[i] = math.exp(-y[i]*c)
+#    rhog[i+1] = M[i+1]/(0.144*3.14*Db[i]**3 )
 
+#    N[i+1] = rhog[i+1]*Na/235
+#    if i >0:
+#        M[i] =  M[0] * math.exp(-c*y[i])
+#        rhog[i] = M[i]/(0.144*3.14*Db[i]**3 )
 
-
-
-
-    rhol[i+1] = 949-0.223*(T[i+1]-273.15)-1.7E-5*(T[i+1]-273.15)**2 #density of sodium
-
-    v[i+1] = v[i] + dt*(6/(rhol[i]*3.14*Db[i]**3))*(  (3.14*Db[i]**3 *g*(rhol[i]-rhog[i]))/6 - 12*3.14*Db[i]*v[i]) #velocity of bubble
-
-    y[i+1] = (i+1)*dt*v[i+1]
 
     if y[i]>5:
         break
@@ -169,6 +157,20 @@ plt.xlabel('time(seconds)',color='blue',size=14)
 plt.ylabel('diameter (meter)',color='blue',size=14)
 plt.legend()
 plt.savefig('diameter')
+plt.figure()
+plt.plot(y[0:i]*100,np.log(1/(RF[0:i])),label='release fraction',color='blue')
+plt.xlabel('Time',color='blue',size=14)
+plt.ylabel('Decontamination  fraction',color='blue',size=14)
+plt.legend()
+plt.savefig('Decontamination')
+
+plt.figure()
+plt.plot(t[0:i],(RF[0:i]),label='release fraction',color='blue')
+plt.xlabel('Time',color='blue',size=14)
+plt.ylabel('Release fraction',color='blue',size=14)
+plt.legend()
+plt.savefig('Release')
+
 '''
 plt.figure()
 plt.plot(t[:-4],T[:-4],label = 'Temperature',color='blue')
